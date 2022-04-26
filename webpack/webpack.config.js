32a4b55affdf489f2416336e111ad2fb0988afe1
Alexander Celis <alexcelis019@gmail.com>
2022-04-26 16:03:36 -0500

    config webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,37 @@
+//require html-webpack-plugin
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//require mini-css-extract-plugin
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  module:{
-    
-  }
-}
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      } /* rule html */,
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      } /* rule css */,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
