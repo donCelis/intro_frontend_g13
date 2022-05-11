11c9b802042fc1527415dc631ef0665c95e8bcaa
Alexander Celis <alexcelis019@gmail.com>
2022-05-10 20:03:07 -0500

    add: crear un componente en react

--- react-app-vite/src/components/App.jsx
+++ react-app-vite/src/components/App.jsx
@@ -1,45 +1,16 @@
-import { useState } from 'react'
-import logo from '@/assets/logo.svg'
 import '@/styles/App.css'
+import { Contador } from './Contador'
 
-function App () {
-  const [count, setCount] = useState(0)
+// memo -> los componentes en react deben iniciar en mayus
+export const App = () => {
+  // lógica del componente
+  const userName = 'Alexander'
 
+  // vista del componente (jsx)
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <p>Bievenido al master in code, {userName}</p>
+      <Contador />
     </div>
   )
 }
-
-export default App
--- react-app-vite/src/main.jsx
+++ react-app-vite/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '@/components/App'
+import { App } from '@/components/App'
 import '@/styles/index.css'
 
-const app = document.getElementById('app')
-const container = createRoot(app)
+const root = document.getElementById('app')
+const container = createRoot(root)
 
 container.render(
   <StrictMode>
