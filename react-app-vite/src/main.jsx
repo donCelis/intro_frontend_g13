7a3406cf8b751c5cb24737505f1d7b67d72a6f37
Alexander Celis <alexcelis019@gmail.com>
2022-05-10 19:23:37 -0500

    mod: resetear el proyecto

--- react-app-vite/src/main.jsx
+++ react-app-vite/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from '@/components/App'
+import '@/styles/index.css'
 
 const app = document.getElementById('app')
 const container = createRoot(app)
