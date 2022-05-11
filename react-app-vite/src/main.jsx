14501e33ad8b6688a3c4348329ba5d662d7f92c0
Alexander Celis <alexcelis019@gmail.com>
2022-05-10 19:12:38 -0500

    add: crear un proyecto de react con vite

---
+++ react-app-vite/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const app = document.getElementById('app')
+const container = createRoot(app)
+
+container.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
